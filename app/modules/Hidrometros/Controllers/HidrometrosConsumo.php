35f646e78e8d31eecdc60def6f65046562102377
Felideo <felideo@gmail.com>
2016-02-02 23:36:09 -0200

    HIDROSIS - menu e paginas de hidrometros de controle e consumo!

---
+++ app/modules/Hidrometros/Controllers/HidrometrosConsumo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Hidrometros\Controllers;
+
+use \App;
+use \View;
+use \Menu;
+use \Admin\BaseController;
+
+class HidrometrosConsumo extends BaseController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        Menu::get('admin_sidebar')->setActiveMenu('group');
+    }
+
+    public function index()
+    {
+        $this->data['title'] = 'Hidrometros de Consumo';
+        /** render the template */
+        View::display('@hidrometros/consumo/index.twig', $this->data);
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
