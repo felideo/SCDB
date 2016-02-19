751b01c680b922a5a4674b0f3869b1d2cb6aa641
Felideo <felideo@gmail.com>
2016-02-19 00:13:28 -0200

    HIDROSIS - explorando essa bosta!

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
-    include_once('../vendor/funcoes.php');
+    // include_once('../vendor/funcoes.php');
+    $app = require('../vendor/funcoes.php');
 	$app = require '../app/bootstrap/start.php';
 	$app->run();
