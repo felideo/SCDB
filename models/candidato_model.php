59a14d6537e5813bc9520740ccbba2f86dacd12e
Felideo <felideo@gmail.com>
2016-10-05 10:28:50 -0300

    DEV - NeuroSis * inicio desenvolvimento modulo de pacientes!

---
+++ models/candidato_model.php
@@ -0,0 +1,26 @@
+<?php
+namespace Models;
+
+use Libs;
+
+class Candidato_Model extends \Libs\Model {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function create($table, $data){
+		$data += [
+			'ativo' => 1,
+		];
+
+		return $this->get_insert($table, $data);
+	}
+
+	public function update($table, $id, $data){
+		$data += [
+			'ativo' => 1,
+		];
+
+		return $this->db->update($table, $data, "`id` = {$id}");
+	}
+}
