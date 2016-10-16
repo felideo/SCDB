f745b16c27d8357535ddea092dc41d85082d2afa
Felideo <felideo@gmail.com>
2016-10-16 20:36:10 -0200

    Ajuste pagina de configurações!

--- views/modulo/form/form.php
+++ views/modulo/form/form.php
@@ -19,11 +19,15 @@
                 </div>
             </div>
             <div class="row-fluid">
-                <div class="form-group span4">
-                    <label>Submenu</label>
-                    <input class="form-control" name="<?php echo $this->modulo['modulo']; ?>[submenu]" value="<?php if(isset($this->cadastro)){echo $this->cadastro['submenu'];} ?>" >
+                <div class="form-group span3">
+                     <label>Submenu</label>
+                     <input class="form-control" name="<?php echo $this->modulo['modulo']; ?>[submenu]" value="<?php if(isset($this->cadastro)){echo $this->cadastro['submenu'];} ?>" >
                 </div>
-                <div class="form-group span2">
+                <div class="form-group span3">
+                    <label>Submenu Icone</label>
+                    <input class="form-control" name="<?php echo $this->modulo['submenu_icone']; ?>[icone]" value="<?php if(isset($this->cadastro)){echo $this->cadastro['submenu_icone'];} ?>" >
+                </div>
+                <div class="form-group span3">
                     <label>Hierarquia</label>
                     <input class="form-control" name="<?php echo $this->modulo['modulo']; ?>[hierarquia]" value="<?php if(isset($this->cadastro)){echo $this->cadastro['hierarquia'];} ?>" required>
                 </div>
