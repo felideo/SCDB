02fa717beb2e9d02d5e736c7cea41aee72a490d9
Felideo <felideo@gmail.com>
2016-10-16 21:58:04 -0200

    DEV/FIX - SianiMVCBasa * realocação de funções create e update na model * remoção de porcarias * ajuste de models

--- libs/Model.php
+++ libs/Model.php
@@ -10,18 +10,28 @@ abstract class Model {
 		$this->db = new Database(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
 	}
 
+	public function create($table, $data){
+		$data += [
+			'ativo' => 1,
+		];
+
+		return $this->get_insert($table, $data);
+	}
+
 	public function get_insert($table, $data) {
 		return $this->db->insert($table, $data);
 	}
 
-	public function load_full_list($table){
-		$full_list = 'SELECT * FROM ' . $table;
-		return $this->db->select($full_list);
+	public function update($table, $id, $data){
+		$data += [
+			'ativo' => 1,
+		];
+
+		return $this->db->update($table, $data, "`id` = {$id}");
 	}
 
-	public function load_active_list($table) {
-		$select = 'SELECT * FROM ' . $table . ' WHERE ativo = 1';
-		return $this->db->select($select);
+	public function update_relacao($table, $where, $id, $data){
+		return $this->db->update($table, $data, "`{$where}` = {$id}");
 	}
 
 	public function delete($table, $id) {
@@ -46,6 +56,17 @@ abstract class Model {
 		return $result;
 	}
 
+
+	public function load_full_list($table){
+		$full_list = 'SELECT * FROM ' . $table;
+		return $this->db->select($full_list);
+	}
+
+	public function load_active_list($table) {
+		$select = 'SELECT * FROM ' . $table . ' WHERE ativo = 1';
+		return $this->db->select($select);
+	}
+
 	public function full_load_by_id($table, $id){
 		$query = 'SELECT * FROM ' . $table
 			. ' WHERE ID = ' . $id;
@@ -59,6 +80,7 @@ abstract class Model {
 		return $this->db->select($query);
 	}
 
+
 	public function update_relacao($table, $where, $id, $data){
 		return $this->db->update($table, $data, "`{$where}` = {$id}");
 	}
@@ -70,5 +92,6 @@ abstract class Model {
 	public function update($table, $id, $data){
 		return $this->db->update($table, $data, "`id` = {$id}");
 	}
+
 
 }
