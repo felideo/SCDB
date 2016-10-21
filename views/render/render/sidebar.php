261b45793c445067c1ed2645f60a971ee9fa33b6
Felideo <felideo@gmail.com.com>
2016-10-21 21:00:51 -0200

    DEV - SianiMVCBase * finalização do crud de hierarquias * alteração da logica de login * carregamentos de permissões * alteração da logica de carregamento do menu

--- views/render/render/sidebar.php
+++ views/render/render/sidebar.php
@@ -35,10 +35,6 @@
 		<!-- /.dropdown -->
 	</ul>
 	<!-- /.navbar-top-links -->
-<<<<<<< HEAD
-=======
-
->>>>>>> 5b3d3f3... DEV/MOD SianiMVCBase
 	<div class="navbar-default sidebar" role="navigation">
         <div class="sidebar-nav navbar-collapse">
 			<ul class="nav" id="side-menu">
