9c74f8bdc6c5e1763ddef80e59727a5bc6adf310
Felideo <felideo@gmail.com.com>
2016-10-25 17:03:04 -0200

    MOD - SianiMVCBase * desconsiderar verificação de permissoes para super-admin

--- util/Permission.php
+++ util/Permission.php
@@ -4,11 +4,13 @@ use Libs;
 
 class Permission {
 	public static function check($modulo, $permissao) {
-		if(empty($_SESSION['permissoes'][$modulo]) || empty($_SESSION['permissoes'][$modulo][$permissao])){
-			$view = new Libs\View();
-			$view->alert_js('Vocã não possui permissão para efetuar esta ação...', 'erro');
-			header('location: ' . URL . $modulo);
-			exit;
+		if($_SESSION['usuario']['super_admin'] != 1){
+			if(empty($_SESSION['permissoes'][$modulo]) || empty($_SESSION['permissoes'][$modulo][$permissao])){
+				$view = new Libs\View();
+				$view->alert_js('Vocã não possui permissão para efetuar esta ação...', 'erro');
+				header('location: ' . URL . $modulo);
+				exit;
+			}
 		}
 	}
 }
