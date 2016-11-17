c2e558c5c23c0df76249c1edc697f3c34cce4da9
Felideo <felideo@gmail.com.com>
2016-11-16 22:39:44 -0200

    Nem sei o que fiz aqui

--- config.php
+++ config.php
@@ -3,24 +3,24 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 // Sempre use barra (/) no final das URLs
-// define('URL', 'http://teste.localhost/neurosis/');
+define('URL', 'http://teste.localhost/neurosis/');
 
-define('URL', 'http://felideo.com.br/neurosis/');
+// define('URL', 'http://felideo.com.br/neurosis/');
 
 define('LIBS', 'libs/');
 
 // Configuração com Banco de Dados
-// define('DB_TYPE', 'mysql');
-// define('DB_HOST', '127.0.0.1');
-// define('DB_NAME', 'NeuroSis');
-// define('DB_USER', 'root');
-// define('DB_PASS', 'lilith');
-
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'mysql.hostinger.com.br');
-define('DB_NAME', 'u595159613_neuro');
-define('DB_USER', 'u595159613_neuro');
-define('DB_PASS', '20lilith88');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'NeuroSis');
+define('DB_USER', 'root');
+define('DB_PASS', 'lilith');
+
+// define('DB_TYPE', 'mysql');
+// define('DB_HOST', 'mysql.hostinger.com.br');
+// define('DB_NAME', 'u595159613_neuro');
+// define('DB_USER', 'u595159613_neuro');
+// define('DB_PASS', '20lilith88');
 
 define('APP_NAME', 'NeuroSis');
 
