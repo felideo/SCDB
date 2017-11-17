b3c7d61cbdc612dfb41e09430978fea2dda704f1
felideo <felideo@gmail.com>
2017-11-16 23:21:51 -0200

    DEV - SWDB * crud de capus * crud de curso * ajustes!

---
+++ modulos/curso/model/curso.php
@@ -0,0 +1,22 @@
+<?php
+namespace Model;
+use Libs;
+
+class Curso extends \Libs\Model {
+	public function buscar_curso($busca){
+		$select = "SELECT"
+			. " 	curso.id,"
+			. " 	curso.curso"
+			. " FROM"
+			. " 	curso curso"
+			. " WHERE"
+			. " 	curso.curso LIKE '%{$busca['nome']}%'"
+			. " AND curso.ativo = 1";
+
+			if(isset($busca['page_limit'])){
+				$select .= " LIMIT {$busca['page_limit']}";
+			}
+
+		return $this->db->select($select);
+	}
+}
