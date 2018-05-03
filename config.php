f67ad229acc94c59fbe3eb0f31e69a753c1daf7d
Felideo <felideo@gmail.com>
2018-05-03 01:42:43 +0000

    DEV - SWDB * ajustes!

--- config.php
+++ config.php
@@ -13,9 +13,9 @@ define('LIBS', 'libs/');
 // Configuração com Banco de Dados
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'SWDB');
-define('DB_USER', 'root');
-define('DB_PASS', 'lilith');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
 define('DEVELOPER', true);
 define('PREVENT_CACHE', true);
