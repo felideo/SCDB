e7f5162d9507ab0e6b6413e8c19f30409f5b7159
Felideo <diego.felideo@betalabs.com.br>
2018-06-07 14:12:59 -0300

    FIX - SWDB * ajuste de deploy 2

--- automatic_deploy.sh
+++ automatic_deploy.sh
@@ -3,13 +3,13 @@ echo "******************** Deploy Automatico SWDB ********************<br>"
 echo "<br>"
 
 cd "/www/swdb/"
-cp /www/swdb/config.php ~/config.php.swdb.back
+# cp /www/swdb/config.php ~/config.php.swdb.back
 git checkout -- .
 git fetch --all
 git reset --hard origin/master
 git pull --rebase
-cp ~/config.php.swdb.back /www/swdb/
-mv config.php.swdb.back config.php
+# cp ~/config.php.swdb.back /www/swdb/
+# mv config.php.swdb.back config.php
 
 echo "<br>"
 echo "<br>"
