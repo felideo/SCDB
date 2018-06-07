c170f7712c7cbdfb381b1c3fab33a95e0555c4c0
Felideo <diego.felideo@betalabs.com.br>
2018-06-07 14:02:52 -0300

    FIX - SWDB * escondendo erros fora do modo developer!

--- index.php
+++ index.php
@@ -4,5 +4,11 @@ require 'config.php';
 require 'util/auth.php';
 require 'vendor/autoload.php';
 
+if(empty(DEVELOPER)){
+	ini_set('display_errors', 0);
+	ini_set('display_startup_errors', 0);
+	error_reporting(0);
+}
+
 session_start();
 new Framework\BigBang();
