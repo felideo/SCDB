037b217067e068408ba54dbbacb27a09435c281f
Felideo <felideo@gmail.com>
2018-11-30 16:53:35 -0200

    FIX - SWDB * adicionando namespace na clace de exception por conflito no servidor!

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
+
 require 'framework/Error.php';
 require 'util/functions.php';
 require 'config.php';
 require 'util/auth.php';
 require 'vendor/autoload.php';
-
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
 
 if(((isset($_SESSION['mostrar_erros']) && !empty($_SESSION['mostrar_erros'])) && $_SESSION['mostrar_erros'] == 'habilitado') || (defined('DEVELOPER')) && !empty(DEVELOPER)){
     error_reporting(E_ALL);
