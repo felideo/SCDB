7d425c4f51d260f7f9d0ab18fb0c22e45155b7a7
Felideo <felideo@gmail.com>
2018-11-30 18:57:21 +0000

    PRODUCAO - SWDB * alterações de deploy em produção!

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
+error_reporting(E_ALL);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
 
 require 'framework/Error.php';
 require 'util/functions.php';
 require 'config.php';
+
 require 'util/auth.php';
 require 'vendor/autoload.php';
 
